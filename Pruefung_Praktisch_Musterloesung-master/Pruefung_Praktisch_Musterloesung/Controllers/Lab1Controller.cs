8e98bd24852fec0c3544e182c84933bf26a8428d
Martin H <mhoeger@gmx.ch>
2018-01-19 09:12:31 +0100

    Aufgabe 1 - Code

--- Pruefung_Praktisch_Musterloesung-master/Pruefung_Praktisch_Musterloesung/Controllers/Lab1Controller.cs
+++ Pruefung_Praktisch_Musterloesung-master/Pruefung_Praktisch_Musterloesung/Controllers/Lab1Controller.cs
@@ -63,42 +63,60 @@ namespace Pruefung_Praktisch_Musterloesung.Controllers
 
         public ActionResult Detail()
         {
-            var file = Request.QueryString["file"];
-            var type = Request.QueryString["type"];
+            // Don't do unsecure URL parameter thing
+            //var file = Request.QueryString["file"];
+            var filehash = Request.QueryString["hashed_file"]; // Hashed filename
 
-            if (string.IsNullOrEmpty(file))
+            List<string> folder = new List<string>();
+
+            foreach (var file1 in folder)
             {
-                file = "Lion1.jpg";
+
+                // Check, which file is equal the hash
+                if (FormsAuthentication.HashPasswordForStoringInConfigFile(file1.Trim(), "md5") == filehash)
+                {
+                    var file = "";
+
+                    var type = Request.QueryString["type"];
+
+                    if (string.IsNullOrEmpty(file))
+                    {
+                        file = "Lion1.jpg";
+                    }
+                    if (string.IsNullOrEmpty(type))
+                    {
+                        file = "lions";
+                    }
+
+                    var relpath = "~/Content/images/" + type + "/" + file;
+
+                    List<List<string>> fileUriItem = new List<List<string>>();
+                    var path = Server.MapPath(relpath);
+
+                    if (System.IO.File.Exists(path))
+                    {
+                        var scheme = Request.Url.Scheme;
+                        var host = Request.Url.Host;
+                        var port = Request.Url.Port;
+                        var absolutepath = Request.Url.AbsolutePath;
+
+                        var filename = Path.GetFileName(file);
+                        var imageuri = scheme + "://" + host + ":" + port + "/Content/images/" + type + "/" + filename;
+
+                        var urilistelement = new List<string>();
+                        urilistelement.Add(filename);
+                        urilistelement.Add(imageuri);
+                        urilistelement.Add(type);
+
+                        fileUriItem.Add(urilistelement);
+                    }
+
+
+                    return View(fileUriItem);
+                }
             }
-            if (string.IsNullOrEmpty(type))
-            {
-                file = "lions";
-            }
 
-            var relpath = "~/Content/images/" + type + "/" + file;
-
-            List<List<string>> fileUriItem = new List<List<string>>();
-            var path = Server.MapPath(relpath);
-
-            if (System.IO.File.Exists(path))
-            {
-                var scheme = Request.Url.Scheme;
-                var host = Request.Url.Host;
-                var port = Request.Url.Port;
-                var absolutepath = Request.Url.AbsolutePath;
-
-                var filename = Path.GetFileName(file);
-                var imageuri = scheme + "://" + host + ":" + port + "/Content/images/" + type + "/" + filename;
-
-                var urilistelement = new List<string>();
-                urilistelement.Add(filename);
-                urilistelement.Add(imageuri);
-                urilistelement.Add(type);
-
-                fileUriItem.Add(urilistelement);
-            }
-            
-            return View(fileUriItem);
+            return View();
         }
     }
 }
